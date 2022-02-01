b5f1e8b42a09d3b449769c534eda9e2e0b4cde7c
M Isa Al Anshori <isabungsu@gmail.com>
2022-02-01 18:18:25 +0700

    delete everything except for very basic code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-**Stable Branch**
+**Rewrite Branch**
 # Serial over LoRa
-A full serial line connection between 2 devices using LoRa. Currently the code is set up for 1200bps with a 250bytes buffer, in theory we should be able to get up to 2000bps safely without extra buffering code that i would need to figure out first. Though Linux console only works at 300bps currently (not yet sure why)
+A full serial line connection between 2 devices using LoRa.
 
 The hardware is 2 pairs of a Wemos ESP8266 module connected to a Ra-02 SX1278 module with the following pin connection
 
@@ -24,25 +24,5 @@ The hardware is 2 pairs of a Wemos ESP8266 module connected to a Ra-02 SX1278 mo
 ## Libraries used
 - [sandeepmistry/arduino-LoRa](https://github.com/sandeepmistry/arduino-LoRa)
 
-## Things I wanna (try to) do
-- Address Filtering
-- Encryption
-- Error Correction
-- Resending (most) Lost Packets
-- Re-Configuration Packets (to change configuration on the fly)
-- Secondary Serial connection for control and configuration? (using SoftwareSerial)
-- Make it compatible with the Arduino Serial library with Stream methods
-
-## Things I need to fix
-- The damn thing transmits 100% of the time and basically just jams the 433mhz frequency.
-- Some reliability problem like random crashes etc.
-- Linux console doesnt seem to work at baud rates other than 300bps. I tested 600 and 1200 bps and it seems to just be sending/receiving garbled data. But even at 300bps, its actually not super terrible. Though regular data/text transmission seem to work perfectly at these higher baud rates, even when both sides have data being sent at the same time. 
-
-## Misc
-Command I used to run a linux shell/console on a ttyUSB:
-```
-sudo getty -L **baudrate** **tty** **terminal type** -8 -a **username**
-
-For example:
-sudo getty -L 600 ttyUSB0 vt102 -a adminispwd
-```
+## To do
+Make the code better than the previous code...
--- SoLR.ino
+++ SoLR.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+#define CONFIG
+#ifdef CONFIG
 // Pin Configuration
 #define ss 15
 #define rst 16
@@ -11,127 +13,43 @@
 
 // Serial Configuration
 #define SoLR_BAUD 1200
-#define SoLR_BUFSIZE 250
-
+#define SoLR_BUFSIZE 5120
 
 // LoRa Configuration
 #define SoLR_FREQ 433E6
+#define SoLR_GAIN 6
+#define SoLR_TXPOWER 20
+#define SoLR_SPREAD 12
+#define SoLR_BANDWIDTH 125E3
+#define SoLR_CR 8
 
 // Connection Configuration
-#define SoLR_CONN_TO 1000UL
-#define SoLR_BEACON_DELAY 1000UL
+#define SoLR_CONN_TO 2000UL
+#define SoLR_BEACON_DELAY 1750UL
+#define SoLR_PACKET_DELAY 1UL
 
-#ifdef SoLR_HOST
-byte localAddress = 0xDD;     // address of this device
-byte remoteAddress = 0xBB;      // destination to send to
-#else
-byte localAddress = 0xBB;     // address of this device
-byte remoteAddress = 0xDD;      // destination to send to
+byte channelID  = 0xFB;
+
 #endif
-
-byte charBuffer[SoLR_BUFSIZE] = {0};
-int bufferIndex = 0;
-
-
-unsigned long lastRecvTime = 0;
-unsigned long lastBeacTime = 0;
-unsigned long lastSendTime = 0;
-
-
-// Data Format
-#define headerLength 3 
-// 1: destination, 
-// 2: localAddress, 
-// 3: packet type (0x00: beacon, 0x01: regular data)
-// rest: data
-
-void sendBuffer() {
-  LoRa.beginPacket();                    // start packet
-  LoRa.write(remoteAddress);             // add destination address
-  LoRa.write(localAddress);              // add sender address
-  LoRa.write(0x01);                      // add packet type (type 0x01: regular data)
-//  Serial.write(charBuffer, bufferIndex+1);
-//  Serial.println();
-  LoRa.write(charBuffer, bufferIndex+1); // add payload
-  memset(charBuffer, 0, SoLR_BUFSIZE);
-  bufferIndex = 0;
-  LoRa.endPacket(true);                      // finish packet and send it asyncronously
-  lastSendTime = millis();
-}
-
-void sendBeacon() {
-  LoRa.beginPacket();                    // start packet
-  LoRa.write(remoteAddress);             // add destination address
-  LoRa.write(localAddress);              // add sender address
-  LoRa.write(0x00);                      // add packet type (type 0x00: beacon)
-  LoRa.write(0xff);                      // add payload
-  LoRa.endPacket();                      // finish packet and send it
-  lastBeacTime = millis();
-}
-
-void receiveData(int packetSize) {
-  if ((packetSize < headerLength + 1) || packetSize > SoLR_BUFSIZE + headerLength) return;    // if there isn't valid packet, return
-  byte recipient = LoRa.read();
-  byte sender = LoRa.read();
-  byte type = LoRa.read();
-  
-  if (recipient != localAddress || sender != remoteAddress) return;
-  
-  byte serialBuffer[SoLR_BUFSIZE] = {0};
-  int serialBufferLength = LoRa.available();
-  LoRa.readBytes(serialBuffer, serialBufferLength);
-  
-  switch (type) {
-    case 0x00: // if packet is a beacon
-//      Serial.println("Beacon Received!");
-      break;
-      
-    case 0x01: // if packet is a regular data
-      Serial.write(serialBuffer, serialBufferLength);
-      break;
-      
-    default: // if packet type is invalid
-      return;
-  }
-  
-  lastRecvTime = millis();
-  sendBuffer();
-}
-
-void loadToBuffer() {
-  int availableBytes = Serial.available();
-  int toRead = 0;
-  if (availableBytes >= SoLR_BUFSIZE - bufferIndex) {
-    toRead = SoLR_BUFSIZE - bufferIndex;
-  } else {
-    toRead = availableBytes;
-  }
-  Serial.readBytes(&charBuffer[bufferIndex], toRead);
-  bufferIndex += toRead;
-}
 
 void setup() {
   Serial.begin(SoLR_BAUD);          // initialize serial
   while (!Serial);
-
+  
   LoRa.setPins(ss, rst, dio0);      // set CS, reset, IRQ pin
   if (!LoRa.begin(SoLR_FREQ)) {     // initialize ratio at freq hz
     Serial.println("LoRa init failed. Check your connections.");
     while (true);                   // if failed, do nothing
+  } else {
+  LoRa.setTxPower(SoLR_TXPOWER);
+  LoRa.setGain(SoLR_GAIN);
+  LoRa.setSpreadingFactor(SoLR_SPREAD);
+  LoRa.setSignalBandwidth(SoLR_BANDWIDTH);
+  LoRa.setCodingRate4(SoLR_CR);
   }
   
 }
 
 void loop() {
-  if (bufferIndex < SoLR_BUFSIZE) {
-    loadToBuffer();
-  }
-  #ifdef SoLR_HOST
-  if ((millis() - lastRecvTime > SoLR_CONN_TO) && (millis() - lastBeacTime > SoLR_BEACON_DELAY)) {
-//    Serial.println("Sending Beacon...");
-    sendBeacon();
-  }
-  #endif
-  receiveData(LoRa.parsePacket());
 
 }
