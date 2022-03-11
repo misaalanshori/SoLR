ed8ea87119a4e61459bf8063204c0e9639d45000
M Isa Al Anshori <isabungsu@gmail.com>
2022-03-11 19:40:12 +0700

    working at low bandwidth. still crashes a lot

--- SoLR.ino
+++ SoLR.ino
@@ -6,7 +6,7 @@
 #ifdef SoLR_CONFIG
 // Pin Configuration
 #define ss 15
-#define rst 16
+#define rst 16 //16
 #define dio0 4
 
 // Comment out the following line when compiling for client
@@ -14,7 +14,7 @@
 #define SoLR_DEBUG
 
 // Serial Configuration
-#define SoLR_BAUD 1200
+#define SoLR_BAUD 9600
 #define SoLR_BUFSIZE 5120
 
 // LoRa Configuration
@@ -26,13 +26,13 @@
 #define SoLR_CR 8
 
 // Connection Configuration
-#define SoLR_CONN_TO 2000UL
-#define SoLR_BEACON_DELAY 1750UL
+#define SoLR_CONN_TO 2250UL
+#define SoLR_BEACON_DELAY 2500UL
 #define SoLR_PACKET_DELAY 1UL
 #define SoLR_HEADER_SIZE 3
 #define SoLR_ID 0xFB
 #define SoLR_PACKET_FOOTER 0x4A
-#define SoLR_MAX_PAYLOAD_SIZE 32
+#define SoLR_MAX_PAYLOAD_SIZE 16
 
 #endif
 
@@ -45,67 +45,69 @@
 LoopbackStream buffer(SoLR_BUFSIZE);
 
 byte sendingBuf[SoLR_MAX_PAYLOAD_SIZE];
+byte recvBuf[SoLR_MAX_PAYLOAD_SIZE];
 
 unsigned long long lastRecvTime = 0;
 unsigned long long lastSendTime = 0;
 
-bool transmitReady = true;
-bool sendAvailable = false;
-int availableBytes = 0;
-
+bool sendAvailable = true;
+short sendingNow = 0;
+int sendSize = 0;
 void sendBuffer(byte type, byte *buf, byte size) {
   if (size > SoLR_MAX_PAYLOAD_SIZE) return;
-  transmitReady = false;
 
   LoRa.beginPacket();
 
   // Write Header
   LoRa.write(SoLR_ID);
   LoRa.write(type);
-  LoRa.write(size);
+  LoRa.write(size);   
 
   // Write Data
-  LoRa.write(buf, size);
+  if (size > 0) {
+    LoRa.write(buf, size);
+  }
+  
 
   // Write Footer
   LoRa.write(SoLR_PACKET_FOOTER);
-
-  LoRa.endPacket(true);
-
+  LoRa.endPacket();
   lastSendTime = millis();
-
+  sendAvailable = false;
+  LoRa.receive();
 }
 
-void onReceive() {
-  if (LoRa.available() < SoLR_HEADER_SIZE) return;
+void onReceive(int packetSize) {
   
+  if (LoRa.available() < SoLR_HEADER_SIZE) return;
+
   byte cID = LoRa.read();
   if (cID != SoLR_ID) return;
   byte type = LoRa.read();
   byte size = LoRa.read();
   if (size > SoLR_MAX_PAYLOAD_SIZE) return;
 
-  byte dataBuf[size];
-  LoRa.readBytes(dataBuf, size);
-  byte footer = LoRa.read();
-  if (footer != SoLR_PACKET_FOOTER) return;
+  if (size > 0 ) {
+    LoRa.readBytes(recvBuf, size);
+  }
 
+  byte footer = LoRa.read();
+
+  if (footer != SoLR_PACKET_FOOTER) return;
   switch (type)
   {
   case SoLR_BEACON_PACKET:
     break;
   
   case SoLR_DATA_PACKET:
-    Serial.write(dataBuf, size);
+    Serial.write(recvBuf, size);
     break;
 
   default:
     return;
   }
-
   lastRecvTime = millis();
   sendAvailable = true;
-  
   
 }
 
@@ -114,15 +116,6 @@ void loadToBuffer(int availableBytes) {
   byte bf[availableBytes];
   Serial.readBytes(bf, availableBytes);
   buffer.write(bf, availableBytes);
-}
-
-void setTransmitReady() {
-  transmitReady = true;
-  LoRa.receive();
-}
-
-void setPacketAvailable(int size) {
-  availableBytes = size;
 }
 
 void setup() {
@@ -139,31 +132,31 @@ void setup() {
   LoRa.setSpreadingFactor(SoLR_SPREAD);
   LoRa.setSignalBandwidth(SoLR_BANDWIDTH);
   LoRa.setCodingRate4(SoLR_CR);
-
-  LoRa.onTxDone(setTransmitReady);
-  LoRa.onReceive(setPacketAvailable);
+  LoRa.receive();
   }
   
 }
 
 void loop() {
+
   loadToBuffer(Serial.available());
 
-  if ((millis() - lastSendTime > SoLR_PACKET_DELAY ) && transmitReady && sendAvailable) {
-    int sendSize = buffer.available() > SoLR_MAX_PAYLOAD_SIZE ? SoLR_MAX_PAYLOAD_SIZE : buffer.available();
+  
+  if ((millis() - lastSendTime > SoLR_PACKET_DELAY ) && sendAvailable) {
+
+    sendSize = buffer.available() > SoLR_MAX_PAYLOAD_SIZE ? SoLR_MAX_PAYLOAD_SIZE : buffer.available();
     buffer.readBytes(sendingBuf, sendSize);
     sendBuffer(SoLR_DATA_PACKET, sendingBuf, sendSize);
   }
+  if (sendingNow == 0) {
+    onReceive(LoRa.parsePacket());
+  }
+  
 
   #ifdef SoLR_HOST
-  if ((millis() - lastRecvTime > SoLR_CONN_TO) && (millis() - lastSendTime > SoLR_BEACON_DELAY) && transmitReady) {
+  if ((millis() - lastRecvTime > SoLR_CONN_TO) && (millis() - lastSendTime > SoLR_BEACON_DELAY)) {
+
     sendBuffer(SoLR_BEACON_PACKET, sendingBuf, 0);
   }
   #endif
-
-  if (availableBytes) {
-    availableBytes = 0;
-    onReceive();
-  }
-
 }
