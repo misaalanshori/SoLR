9a052ee83b2c6812d7d437b4b644d22ae8ebe4c8
M Isa Al Anshori <isabungsu@gmail.com>
2024-04-06 12:13:07 +0700

    Working quite well at 110bps

--- SoLR.ino
+++ SoLR.ino
@@ -1,8 +1,7 @@
 #include <SPI.h>
-#include <LoRa.h>
+#include <LoRa.h> // sandeepmistry/arduino-LoRa
+#include <LoopbackStream.h> //paulo-raca/ArduinoBufferedStreams
 
-#define CONFIG
-#ifdef CONFIG
 // Pin Configuration
 #define ss 15
 #define rst 16
@@ -12,25 +11,98 @@
 #define SoLR_HOST
 
 // Serial Configuration
-#define SoLR_BAUD 1200
-#define SoLR_BUFSIZE 5120
+#define SoLR_BAUD 110
+#define SoLR_BUFSIZE 8192
 
 // LoRa Configuration
 #define SoLR_FREQ 433E6
 #define SoLR_GAIN 6
 #define SoLR_TXPOWER 20
 #define SoLR_SPREAD 12
-#define SoLR_BANDWIDTH 125E3
-#define SoLR_CR 8
+#define SoLR_BANDWIDTH 250E3
+#define SoLR_CR 7
 
 // Connection Configuration
-#define SoLR_CONN_TO 2000UL
-#define SoLR_BEACON_DELAY 1750UL
+#define SoLR_CONN_TO 2250UL
+#define SoLR_BEACON_DELAY 2500UL
 #define SoLR_PACKET_DELAY 1UL
+#define SoLR_HEADER_SIZE 3
+#define SoLR_ID 0xFB
+#define SoLR_PACKET_FOOTER 0x4A
+#define SoLR_MAX_PAYLOAD_SIZE 16
 
-byte channelID  = 0xFB;
+#define SoLR_BEACON_PACKET 0x00
+#define SoLR_DATA_PACKET 0x01
 
-#endif
+
+LoopbackStream buffer(SoLR_BUFSIZE);
+
+byte sendingBuf[SoLR_MAX_PAYLOAD_SIZE];
+byte recvBuf[SoLR_MAX_PAYLOAD_SIZE];
+
+unsigned long long lastRecvTime = 0;
+unsigned long long lastSendTime = 0;
+
+bool sendAvailable = true;
+short sendingNow = 0;
+int sendSize = 0;
+void sendBuffer(byte type, byte *buf, byte size) {
+  if (size > SoLR_MAX_PAYLOAD_SIZE) return;
+
+  LoRa.beginPacket();
+
+  // Write Header
+  LoRa.write(SoLR_ID);
+  LoRa.write(type);
+  LoRa.write(size);   
+
+  // Write Data
+  if (size > 0) {
+    LoRa.write(buf, size);
+  }
+  
+  // Write Footer
+  LoRa.write(SoLR_PACKET_FOOTER);
+  LoRa.endPacket();
+  lastSendTime = millis();
+  sendAvailable = false;
+  LoRa.receive();
+}
+
+void onReceive(int packetSize) {
+  
+  if (LoRa.available() < SoLR_HEADER_SIZE) return;
+
+  byte connId = LoRa.read();
+  if (connId != SoLR_ID) return;
+  byte type = LoRa.read();
+  byte size = LoRa.read();
+  if (size > SoLR_MAX_PAYLOAD_SIZE) return;
+
+  if (size > 0 ) {
+    LoRa.readBytes(recvBuf, size);
+  }
+
+  byte footer = LoRa.read();
+
+  if (footer != SoLR_PACKET_FOOTER) return;
+  switch (type)
+  {
+  case SoLR_BEACON_PACKET:
+    // Serial.println("BR");
+    break;
+  
+  case SoLR_DATA_PACKET:
+    Serial.write(recvBuf, size);
+    break;
+
+  default:
+    return;
+  }
+  lastRecvTime = millis();
+  sendAvailable = true;
+  
+}
 
 void setup() {
   Serial.begin(SoLR_BAUD);          // initialize serial
@@ -46,10 +118,30 @@ void setup() {
   LoRa.setSpreadingFactor(SoLR_SPREAD);
   LoRa.setSignalBandwidth(SoLR_BANDWIDTH);
   LoRa.setCodingRate4(SoLR_CR);
+  LoRa.receive();
   }
   
 }
 
 void loop() {
 
+  while (Serial.available() > 0 && buffer.availableForWrite() > 0) {
+    buffer.write(Serial.read());
+  }
+  
+  if ((millis() - lastSendTime > SoLR_PACKET_DELAY ) && sendAvailable) {
+    sendSize = min(buffer.available(), SoLR_MAX_PAYLOAD_SIZE);
+    buffer.readBytes(sendingBuf, sendSize);
+    sendBuffer(SoLR_DATA_PACKET, sendingBuf, sendSize);
+  }
+  if (sendingNow == 0) {
+    onReceive(LoRa.parsePacket());
+  }
+  
+  #ifdef SoLR_HOST
+  if ((millis() - lastRecvTime > SoLR_CONN_TO) && (millis() - lastSendTime > SoLR_BEACON_DELAY)) {
+    // Serial.println("BS");
+    sendBuffer(SoLR_BEACON_PACKET, sendingBuf, 0);
+  }
+  #endif
 }
